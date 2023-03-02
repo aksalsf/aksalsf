c9ac4add9fb6a93c8cb94fd701647c27b6ba2e7e
Aksal <43602616+aksalsf@users.noreply.github.com>
2023-03-02 17:04:38 +0700

    refactor(readme): remove quotes

--- index.js
+++ index.js
@@ -9,14 +9,11 @@ const { parse } = require("rss-to-json");
 
 const MEDIUM_RSS_URL = "https://medium.com/feed/@aksalsf";
 const TIMEZONE_OFFSET = 7;
-const ANIME_API_URL = "https://animechan.vercel.app/api/random";
-const PROGRAMMER_API_URL = "https://programming-quotes-api.herokuapp.com/Quotes/random";
 
 (async () => {
 
   const { today, hour } = getCurrentTime();
   const greetings = generateGreetings(hour);
-  const { quote, author } = await getQuotes(hour);
   const snake = `![github contribution grid snake animation](https://raw.githubusercontent.com/aksalsf/aksalsf/output/github-contribution-grid-snake-dark.svg#gh-dark-mode-only)![github contribution grid snake animation](https://raw.githubusercontent.com/aksalsf/aksalsf/output/github-contribution-grid-snake.svg#gh-light-mode-only)`;
 
   const mediumPosts = await fetchMyMediumPosts(MEDIUM_RSS_URL)
@@ -39,8 +36,6 @@ const PROGRAMMER_API_URL = "https://programming-quotes-api.herokuapp.com/Quotes/
     ![Habits Metrics](/habits-metrics.svg)
     ![Social Metrics](/social-metrics.svg)
     ![Achievement Metrics](/achievement-metrics.svg)
-    *"${quote}"* <br>
-    — ${author}
   `;
 
   const content = md.renderInline(text);
@@ -90,65 +85,6 @@ function generateGreetings(time) {
     return goodEvening;
   }
   return goodNight;
-}
-
-async function getQuotes(time) {
-  if (isWeekend()) {
-    if (time >= 4 && time < 23) {
-      return await axios
-        .get(ANIME_API_URL)
-        .then(response => ({
-          quote: response.data.quote,
-          author: `${response.data.character} (${response.data.anime})`
-        }))
-    }
-    return {
-      quote: "Cheers, mate. No work, just chill. Cool, yeh!",
-      author: "Aksal"
-    };
-  }
-  if (time >= 4 && time < 8) {
-    return {
-      quote: "Even though the morning air is good, it is better to not open your Windows.",
-      author: "Aksal"
-    };
-  }
-  if (time >= 8 && time < 11) {
-    return {
-      quote: "Wanna advice? Don't start your day with any meeting. Start with code!",
-      author: "Aksal"
-    };
-  }
-  if (time >= 11 && time < 13) {
-    return {
-      quote: "A good programmer always collaborating, even when having lunch!",
-      author: "Aksal"
-    };
-  }
-  if (time >= 13 && time < 18) {
-    return await axios
-        .get(PROGRAMMER_API_URL)
-        .then(response => ({
-          quote: response.data.en,
-          author: response.data.author
-        }));
-  }
-  if (time >= 18 && time < 20) {
-    return {
-      quote: "Time to home!",
-      author: "Aksal"
-    };
-  }
-  if (time >= 20 && time < 23) {
-    return {
-      quote: "Take a good book to bed with you—books do not snore.",
-      author: "Aksal"
-    };
-  }
-  return {
-    quote: "Have a nice dream!",
-    author: "Aksal"
-  };
 }
 
 function generateFile(contents) {
